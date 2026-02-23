03247e500cf906102c33d774d9011212cd5d98a5
Almusayid <sulthanfaaz@gmail.com>
2026-02-23 17:14:04 +0700

    New

---
+++ Tugas_02_SimplePython/Main.py
@@ -0,0 +1,16 @@
+# main.py
+
+from Menu import Tampilkan_menu
+from input_pesanan import ambil_pesanan
+from pembayaran import pembayaran
+
+print("===== SISTEM KASIR RESTORAN AYAM GEPREK =====")
+menu = Tampilkan_menu()
+
+i = 0
+
+for item in menu.values():
+    i += 1
+    print(f"{i}. {item[0]} - Rp {item[1]}")
+    
+pembayaran(ambil_pesanan(menu))
