b8507f846555621088659647ef419334e4cf276d
sahlaaa <sahlafr10@gmail.com>
2026-02-23 16:22:03 +0700

    progres pembayaran (2)

--- Tugas_02_SimplePython/pembayaran.py
+++ Tugas_02_SimplePython/pembayaran.py
@@ -1,54 +1,23 @@
-konfirmasi = False #konfirmasi agar proses pembayaran berulang saat pembayaran kurang
+def pembayaran(pesanan) :
+    total = 0 
+    konfirmasi = False #konfirmasi agar proses pembayaran berulang saat pembayaran kurang
 
-while konfirmasi == False : 
-    pembayaran = input("input pembayaran : ")
-    grandtotal = 150000
+    for item in pesanan :
+        total += item[2]
 
-    if int(pembayaran) < grandtotal :
-        print("Pembayaran anda kurang!")
-    elif int(pembayaran) == grandtotal :
-        konfirmasi = True
-        kembalian = 0;
-        print("Pembayaran anda sebesar ", int(pembayaran), " berhasil!")
-        print("Kembalian : Rp ", kembalian)
-    else :
-        konfirmasi = True
-        kembalian = int(pembayaran) - grandtotal
-        print("Kembalian : Rp ", kembalian)
+    while konfirmasi == False : 
+        pembayaran = input("input pembayaran : ")
+        grandtotal = 150000
 
-#pembuatan struk
-from datetime import datetime
+        if int(pembayaran) < total :
+            print("Pembayaran anda kurang!")
+        elif int(pembayaran) == total :
+            konfirmasi = True
+            kembalian = 0;
+            print("Pembayaran anda sebesar ", int(pembayaran), " berhasil!")
+            print("Kembalian : Rp ", kembalian)
+        else :
+            konfirmasi = True
+            kembalian = int(pembayaran) - total
+            print("Kembalian : Rp ", kembalian)
 
-print("=================================")
-print("        AYAM GEPREK BERKAH")
-print("=================================")
-
-# Tanggal
-tanggal = datetime.now().strftime("%d-%m-%Y")
-print("Tanggal :", tanggal)
-print("Kasir   : Asep")
-
-print("---------------------------------")
-print("Menu\t\tQty\tSubtotal")
-print("---------------------------------")
-
-# data belanja
-belanja = [
-    ("Ayam Geprek Original", 2, 30000),
-    ("Es Teh", 1, 4000),
-    ("Nasi Putih", 1, 5000)
-]
-
-total = 0
-for item in belanja:
-    print(f"{item[0]}\t{item[1]}\t{item[2]}")
-    total += item[2]
-
-print("---------------------------------")
-print("Total\t\t\t", grandtotal)
-print("Bayar\t\t\t", int(pembayaran))
-print("Kembalian\t\t", kembalian)
-print("---------------------------------")
-print("     Terima Kasih :)")
-print("   Selamat Menikmati!")
-print("=================================")
